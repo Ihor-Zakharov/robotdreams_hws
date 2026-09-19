ad51ac6326214371d5e1b8a90a49604a0f2ce546
Ihor Zakharov <ihor.zakharov@dopplerforge.com>
2026-09-19 19:31:23 +0000

    HW2: tasks 1-3

---
+++ robotdreams/hw2/project_1.srcs/sources_1/new/tb_counter.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps          
+
+module tb_counter;            
+
+    reg  clk, rst, load, en, up_down;
+    reg  [3:0] data_in;
+    wire [3:0] count;
+
+    counter dut (
+      .clk(clk),
+      .rst(rst),
+      .load(load),
+      .data_in(data_in),
+      .en(en),
+      .up_down(up_down),
+      .count(count)
+    );
+
+    initial begin
+      clk = 0;
+      forever #5 clk = ~clk;
+    end
+    
+    initial begin
+        rst=0; load=0; en=0; up_down=0; data_in=0;
+        
+        rst = 1;
+        @(posedge clk);
+        rst = 0;
+
+        load=1; data_in=4'd10;
+        
+        @(posedge clk); #1;
+      
+        if (count === 4'd10)
+            $display("PASS");
+        else 
+            $display("FAIL");
+    end
+
+endmodule
